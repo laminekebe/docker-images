4cd952ca90e5eaeeb51f32260710a9a9de8e257f
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-05-10 08:52:44 +1000

    symlink certificate file into new location to fix Certificate Authority issues in WMS

--- wms/assets/wms-entrypoint.sh
+++ wms/assets/wms-entrypoint.sh
@@ -2,4 +2,7 @@
 set -e
 
 get_wms_config.sh
+# Fix certificate issues
+mkdir -p /etc/pki/tls/certs
+ln -s /etc/ssl/certs/ca-certificates.crt /etc/pki/tls/certs/ca-bundle.crt;
 docker-entrypoint.sh "$@"
